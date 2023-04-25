66968d5e1cf2eac5b17ba61d63e3fcb652bf777e
Acquah <emanuel.molina2k@gmail.com>
2023-04-24 21:40:39 -0700

    add questions and generation of logo from the prompts

---
+++ index.js
@@ -0,0 +1,43 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateLogo = require("./Assets/generateLogo")
+
+const questions = [{
+    message : "Enter up to three characters: ",
+    type : "input",
+    name : "characters",
+},{
+    message : "Pick a color: ",
+    type : "input",
+    name : "color", 
+},{
+    message : "Pick a shape:",
+    type : "list",
+    name : "shape",
+    choices : ["square", "circle", "triangle"],
+},{
+    message : "Pick shape color:",
+    type : "input",
+    name : "shapeColor",
+},{
+}];
+
+// function that writes
+function writeToFile(fileName, data) {
+    const text = generateLogo(data);
+    fs.appendFile(fileName, text, (err) =>
+        err ? console.log(err) : console.log("Successful!!!")
+            );
+}
+
+// function that initializes the app
+function init() {
+    inquirer 
+    .prompt(questions)
+    .then((data) => {
+        console.log(data);
+        const fileNameForLogo = `Assets/logo.svg`;
+        writeToFile(fileNameForLogo, data);
+    });
+}
+init();
